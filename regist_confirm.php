53a16dbf40421ca48b9e79a6be61ea763a4b8e38
kazu0112 <kazuma0112@icloud.com>
2026-08-28 15:13:30 +0900

    入力可能な文字種類を追加

--- regist_confirm.php
+++ regist_confirm.php
@@ -7,36 +7,72 @@ $family_name = $_POST['family_name'];
 if ($family_name == "") {
     echo "名前（姓）が未入力です。";
     $error_count++;
+} else{
+    if (preg_match("/^[ぁ-んー-龥]+$/u", $family_name)){
+} else{
+        echo "名前（姓)に使用できない文字が含まれています。";
+        $error_count++;
+    }
 }
 
 $last_name = $_POST['last_name'];
 if ($last_name == ""){
     echo "名前（名）が未入力です。";
     $error_count++;
+} else{
+    if (preg_match("/^[ぁ-んー-龥]+$/u", $family_name)){
+} else{
+        echo "名前（姓)に使用できない文字が含まれています。";
+        $error_count++;
+    }
 }
 
 $family_name_kana = $_POST['family_name_kana'];
 if ($family_name_kana == ""){
-    echo "カナ（性）が未入力です。";
+    echo "カナ（姓）が未入力です。";
     $error_count++;
+} else{
+    if (preg_match("/^[ァ-ヶー]+$/u", $family_name_kana)) {
+} else {
+        echo "カナ（姓）に使用できない文字が含まれています。";
+        $error_count++;
+    }
 }
 
 $last_name_kana = $_POST['last_name_kana'];
 if ($last_name_kana == ""){
     echo "カナ（名）が未入力です。";
     $error_count++;
+} else{
+     if (preg_match("/^[ァ-ヶー]+$/u", $family_name_kana)) {
+} else {
+        echo "カナ（姓）に使用できない文字が含まれています。";
+        $error_count++;
+    }
 }
 
 $mail = $_POST['mail'];
 if ($mail == ""){
     echo "メールアドレスが未入力です。";
     $error_count++;
+} else{
+    if (preg_match("/^[a-zA-Z0-9]+$/", $password)) {
+} else {
+        echo "パスワードに使用できない文字が含まれています。";
+        $error_count++;
+    }
 }
 
 $password = $_POST['password'];
-if ($family_name == "") {
+if ($password == "") {
     echo "パスワードが未入力です。";
     $error_count++;
+} else {
+    if (preg_match("/^[a-zA-Z0-9@-]+$/", $mail)) {
+} else {
+        echo "メールアドレスに使用できない文字が含まれています。";
+        $error_count++;
+    }
 }
 
 $gender = $_POST['gender'];
@@ -55,24 +91,42 @@ $postal_code = $_POST['postal_code'];
 if ($postal_code == "") {
     echo "郵便番号が未入力です。";
     $error_count++;
+} else {
+    if (preg_match("/^[0-9]+$/", $postal_code)) {
+} else {
+        echo "郵便番号に使用できない文字が含まれています。";
+        $error_count++;
+    }
 }
 
 $prefecture = $_POST['prefecture'];
 if ($prefecture == "") {
     echo "住所（都道府県）が未選択です。";
     $error_count++;
-}
+} 
 
 $address_1 = $_POST['address_1'];
 if ($address_1 == "") {
     echo "住所（市区町村）が未入力です。";
     $error_count++;
+} else {
+    if (preg_match("/^[ぁ-んァ-ヶ一-龥0-9０-９ー 　-]+$/u", $address_1)) {
+} else {
+        echo "住所（市区町村）に使用できない文字が含まれています。";
+        $error_count++;
+    }
 }
 
 $address_2 = $_POST['address_2'];
 if ($address_2 == "") {
     echo "住所（番地）が未入力です。";
     $error_count++;
+} else {
+    if (preg_match("/^[ぁ-んァ-ヶ一-龥0-9０-９ー 　-]+$/u", $address_2)) {
+} else {
+        echo "住所（番地）に使用できない文字が含まれています。";
+        $error_count++;
+    }
 }
 
 $authority = $_POST['authority'];
@@ -110,7 +164,7 @@ if ($authority == 0){
         <p>名前（性）:<?php echo $family_name; ?></p>
         <p>名前（名）:<?php echo $last_name; ?></p>
         <p>カナ（性）:<?php echo $family_name_kana; ?></p>
-        <p>カナ（名）:<?php echo $last_name; ?></p>
+        <p>カナ（名）:<?php echo $last_name_kana; ?></p>
         <p>メールアドレス:<?php echo $mail; ?></p>
         <p>パスワード:<?php echo str_repeat("⚫️", strlen($password)); ?></p>
         <P>性別:<?php echo $gender_text; ?></P>
