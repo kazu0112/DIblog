3e2b511b0eb7728ca15ac04addaa7961a4557727
kazu0112 <kazuma0112@icloud.com>
2026-09-07 17:02:53 +0900

    regist_complete.php作成

---
+++ regist_complete.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>アカウント登録完了</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+        <div class="container">
+            <div class="navigation">
+                ナビゲーションバー
+            </div>
+            <div class="main">
+                <h2>アカウント登録完了画面</h2>
+                    <div class="complete">
+                        <h3>登録完了しました</h3>
+                        <form method="post" action="index.php">
+                            <input type="submit" value="TOPページへ戻る">
+                        </form>
+                    </div>
+            </div>
+            <div class="footer">
+                フッター
+            </div>
+        </div>
+</body>
