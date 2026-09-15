f81b33bf09dd5a1d54761262c672e254a1f4083a
kazu0112 <kazuma0112@icloud.com>
2026-09-15 16:17:43 +0900

    regist_compete.phpにDB登録機能追加

--- regist_complete.php
+++ regist_complete.php
@@ -1,3 +1,42 @@
+<?php
+
+$family_name = $_POST['family_name'];
+$last_name = $_POST['last_name'];
+$family_name_kana = $_POST['family_name_kana'];
+$last_name_kana = $_POST['last_name_kana'];
+$mail = $_POST['mail'];
+$password = $_POST['password'];
+$gender = $_POST['gender'];
+$postal_code = $_POST['postal_code'];
+$prefecture = $_POST['prefecture'];
+$address_1 = $_POST['address_1'];
+$address_2 = $_POST['address_2'];
+$authority = $_POST['authority'];
+
+//データベース接続
+$mysqli = new mysqli("localhost", "root", "root", "lesson01");
+
+if ($mysqli->connect_error) {
+    die("データベース接続に失敗しました。");
+}
+
+$mysqli->set_charset("utf8");
+
+
+// データベースに登録
+$sql = "INSERT INTO account
+        (family_name, last_name, family_name_kana, last_name_kana, mail, password, gender, postal_code, prefecture, address_1, address_2, authority, delete_flag, registered_time, update_time)
+        VALUES
+        ('$family_name', '$last_name', '$family_name_kana', '$last_name_kana', '$mail', '$password', '$gender', '$postal_code', '$prefecture', '$address_1', '$address_2', '$authority', 0, NOW(), NOW())";
+
+$result = $mysqli->query($sql);
+
+if (!$result) {
+    die("登録に失敗しました。");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
